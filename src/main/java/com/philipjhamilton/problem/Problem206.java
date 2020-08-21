0ec6310288117f0e9a9549b9917da73dd6699dc5
Philip Hamilton <pjhamilton4@gmail.com>
2020-08-21 04:04:16 -0400

    solved 206

--- src/main/java/com/philipjhamilton/problem/Problem206.java
+++ src/main/java/com/philipjhamilton/problem/Problem206.java
@@ -11,30 +11,25 @@ public class Problem206 implements Problem<Long>{
         long min = (long)Math.sqrt(1020304050607080900L);
         long max = (long)Math.sqrt(1929394959697989990L);
 
-        for(long i = max; i >= min; i-=10){
+        for(long i = max; i >= min; i--){
             bi = new BigInteger(String.valueOf(i));
 
-            String s = bi.multiply(bi).toString();
+            bi = bi.pow(2);
 
-            if(s.charAt(0) == 1 && s.charAt(2) == 2  && s.charAt(4) == 3 && s.charAt(6) == 4 && s.charAt(8) == 5 && s.charAt(10) == 6
-                    && s.charAt(12) == 7 && s.charAt(14) == 8  && s.charAt(16) == 9  && s.charAt(18) == 0){
-                System.out.print(" | " + i + " |");
-                System.out.println(s);
-                break;
+            if(found(bi)){
+                return i;
             }
+
         }
 
         return null;
     }
 
-    private boolean found(String s){
-        char[] chars = s.toCharArray();
-        // This pains me to write for some reason.
-        //System.out.println(Arrays.toString(chars));
-        System.out.println(chars[0] + "|" + chars[2] + "|" + chars[4] + "|" + chars[6] + "|" + chars[8] + "|" + chars[10] + "|" +
-                chars[12] + "|" + chars[14] + "|" + chars[16] + "|" + chars[18]);
-        if(chars[0] == 1 && chars[2] == 2  && chars[4] == 3 && chars[6] == 4  && chars[8] == 5 && chars[10] == 6
-                && chars[12] == 7 && chars[14] == 8  && chars[16] == 9  && chars[18] == 0){
+    private boolean found(BigInteger s){
+        char[] nr = s.toString().toCharArray();
+
+        if (nr[0] == '1' && nr[2] == '2' && nr[4] == '3' && nr[6] == '4' && nr[8] == '5' && nr[10] == '6'
+                && nr[12] == '7' && nr[14] == '8' && nr[16] == '9' && nr[18] == '0'){
             return true;
         }
 
